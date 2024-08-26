8994e9d8e7505e50f6cd6adbd11425bdfd573c79
gribVolnushka <burgerqueen98@gmail.com>
2024-08-26 17:56:14 +0200

    Fixed formatting and fixed errors and semicolons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-const
-num1 =
-  +prompt
-  (
-'Enter first number'
-)
-
-const
-num2 = +prompt
-(
-'Enter second number'
-)
-
-const num3
-= +
-prompt('Enter third number');
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
 
 alert(num1 + num2 + num3);
